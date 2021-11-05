b3564d707423ed31b6793d9e1757afa518bd0489
Robert Chow <rchow@thoughtworks.com>
2021-11-05 14:08:53 +0000

    Add multiple different keys

--- lib/main.dart
+++ lib/main.dart
@@ -8,16 +8,48 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: TextButton(
-            child: Text('Click Me'),
-            onPressed: () {
-              final player = AudioCache();
-              player.play('note1.wav');
-            },
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              XylophoneKey(1, Colors.red),
+              XylophoneKey(2, Colors.orange),
+              XylophoneKey(3, Colors.yellow),
+              XylophoneKey(4, Colors.green),
+              XylophoneKey(5, Colors.teal),
+              XylophoneKey(6, Colors.blue),
+              XylophoneKey(7, Colors.purple),
+            ],
           ),
         ),
       ),
     );
+  }
+}
+
+class XylophoneKey extends StatelessWidget {
+  final int number;
+  final Color color;
+  final player = AudioCache();
+
+  XylophoneKey(this.number, this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: TextButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateColor.resolveWith((states) => color),
+          padding:
+              MaterialStateProperty.resolveWith((states) => EdgeInsets.zero),
+        ),
+        onPressed: playSound,
+      ),
+    );
+  }
+
+  void playSound() {
+    player.play('note$number.wav');
   }
 }
